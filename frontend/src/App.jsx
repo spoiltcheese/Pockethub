bcdb0aa0c05abffce36974eb6d6279617739f115
spoiltcheese <30766633+spoiltcheese@users.noreply.github.com>
2025-09-09 17:46:26 +0800

    edit template to use tanstack react-query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+
+import UserContext from "./context/user";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Navigate, Route, Routes } from "react-router";
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [accessToken, setAccessToken] = useState(""); // Access token state
+  const [role, setRole] = useState(""); // User role state
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <QueryClientProvider client={queryClient}>
+        <UserContext.Provider
+          value={{ accessToken, setAccessToken, role, setRole }}
+        >
+          <Routes>
+            <Route path="/" element={<Navigate to="/main" />} />
+            <Route path="/login" element={<div>Login Page</div>} />
+            <Route path="/main" element={<div>Main Page</div>} />
+          </Routes>
+        </UserContext.Provider>
+      </QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
