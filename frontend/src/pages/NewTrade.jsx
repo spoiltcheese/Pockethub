0ac7e49f7bde9c5c09150e26f97fe905d49fdda5
spoiltcheese <30766633+spoiltcheese@users.noreply.github.com>
2025-09-14 16:46:44 +0800

    Update NewTrade.jsx

--- frontend/src/pages/NewTrade.jsx
+++ frontend/src/pages/NewTrade.jsx
@@ -4,19 +4,35 @@ import Dropdown from "react-bootstrap/Dropdown";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const NewTrade = () => {
-  const [selectedValue, setSelectedValue] = useState("None");
+  const [selectedRarityLF, setSelectedRarityLF] = useState("None");
+  const [selectedValueLF, setSelectedValueLF] = useState("None");
 
-  const handleSelectRarity = (eventKey) => {
-    setSelectedValue(eventKey);
+  const [selectedRarityTW, setSelectedRarityTW] = useState("None");
+  const [selectedValueTW, setSelectedValueTW] = useState("None");
+
+  const handleSelectRarityLF = (eventKey) => {
+    setSelectedRarityLF(eventKey);
+  };
+
+  const handleSelectValueLF = (eventKey) => {
+    setSelectedValueLF(eventKey);
+  };
+
+  const handleSelectRarityTW = (eventKey) => {
+    setSelectedRarityTW(eventKey);
+  };
+
+  const handleSelectValueTW = (eventKey) => {
+    setSelectedValueTW(eventKey);
   };
 
   useEffect(() => {
-    console.log(`Selected value changed to: ${selectedValue}`);
-  }, [selectedValue]);
+    //console.log(`Selected rarity changed to: ${selectedRarity}`);
+  }, [selectedRarityLF]);
 
   const queryClient = useQueryClient();
 
-  async function getFilteredCards() {
+  async function getFilteredCards(rarity) {
     const url = "http://localhost:5001/api/cards_filtered";
     try {
       const res = await fetch(url, {
@@ -24,7 +40,7 @@ const NewTrade = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ rarity: selectedValue }),
+        body: JSON.stringify({ rarity: rarity }),
       });
 
       const data = await res.json();
@@ -49,18 +65,25 @@ const NewTrade = () => {
   }
 
   const queryCardMedia = useQuery({
-    queryKey: ["cardsByRarity", selectedValue],
-    queryFn: getFilteredCards,
+    queryKey: ["cardsByRarity", selectedRarityLF],
+    queryFn: () => getFilteredCards(selectedRarityLF),
+  });
+
+  const queryCardMedia2 = useQuery({
+    queryKey: ["cardsByRarity2", selectedRarityTW],
+    queryFn: () => getFilteredCards(selectedRarityTW),
   });
 
   return (
     <div className="container">
-      <p className="mt-2">Current Selection: {selectedValue}</p>
+      <p className="mt-2">Looking for: {selectedValueLF}</p>
 
       <DropdownButton
         id="rarity-dropdown"
-        title={`Selected: ${selectedValue}`}
-        onSelect={handleSelectRarity}
+        title={`Selected: ${selectedRarityLF}`}
+        onSelect={handleSelectRarityLF}
+        menuAlign="left"
+        style={{ maxWidth: "100%" }}
       >
         <Dropdown.Item eventKey="1D">1 Diamond</Dropdown.Item>
         <Dropdown.Item eventKey="2D">2 Diamond</Dropdown.Item>
@@ -69,7 +92,14 @@ const NewTrade = () => {
         <Dropdown.Item eventKey="1S">1 Star</Dropdown.Item>
       </DropdownButton>
 
-      <DropdownButton id="filtered-cards-dropdown">
+      <DropdownButton
+        id="filtered-cards-dropdown"
+        title={`Selected: ${selectedValueLF}`}
+        menuAlign="left"
+        style={{ maxWidth: "100%" }}
+        menuVariant="light"
+        onSelect={handleSelectValueLF}
+      >
         {queryCardMedia.isSuccess &&
           queryCardMedia.data &&
           queryCardMedia.data.map((card, id) => (
@@ -78,6 +108,48 @@ const NewTrade = () => {
             </Dropdown.Item>
           ))}
       </DropdownButton>
+
+      <p className="mt-2">Trading with: {selectedValueTW}</p>
+
+      <DropdownButton
+        id="rarity-dropdown"
+        title={`Selected: ${selectedRarityLF}`}
+        onSelect={handleSelectRarityLF}
+        menuAlign="left"
+        style={{ maxWidth: "100%" }}
+      >
+        <Dropdown.Item eventKey="1D">1 Diamond</Dropdown.Item>
+        <Dropdown.Item eventKey="2D">2 Diamond</Dropdown.Item>
+        <Dropdown.Item eventKey="3D">3 Diamond</Dropdown.Item>
+        <Dropdown.Item eventKey="4D">4 Diamond</Dropdown.Item>
+        <Dropdown.Item eventKey="1S">1 Star</Dropdown.Item>
+      </DropdownButton>
+
+      <DropdownButton
+        id="filtered-cards-dropdown"
+        title={`Selected: ${selectedValueTW}`}
+        menuAlign="left"
+        style={{ maxWidth: "100%" }}
+        menuVariant="light"
+        onSelect={handleSelectValueTW}
+      >
+        {queryCardMedia.isSuccess &&
+          queryCardMedia.data &&
+          queryCardMedia.data.map((card, id) => (
+            <Dropdown.Item key={id} eventKey={card.cardname}>
+              {card.cardname}
+            </Dropdown.Item>
+          ))}
+      </DropdownButton>
+
+      <style>{`
+            .dropdown-menu {
+                max-width: 100vw;
+                max-height: 50vh;
+                overflow-y: auto;
+                word-break: break-word;
+            }
+        `}</style>
     </div>
   );
 };
